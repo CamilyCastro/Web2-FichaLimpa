eb9516019265c8afe302b977045efd09336e5c93
Pietro Kaynan Carvalho Lopes da Silva <pietrokaynan1@gmail.com>
2024-11-06 13:19:47 -0300

    criando repositorios de politico e partido

---
+++ fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/repositorios/PoliticoRepositorio.java
@@ -0,0 +1,9 @@
+package edu.ifsp.fichaLimpa.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.ifsp.fichaLimpa.model.Politico;
+
+public interface PoliticoRepositorio extends CrudRepository<Politico, Long>{
+
+}
