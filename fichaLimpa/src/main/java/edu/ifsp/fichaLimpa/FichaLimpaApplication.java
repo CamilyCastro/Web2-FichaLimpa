3dc9637035399a8bc68981f1e5963d2bda66fcc8
Amanda <acampezan@gmail.com>
2024-11-06 09:13:44 -0300

    Projeto Spring

---
+++ fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/FichaLimpaApplication.java
@@ -0,0 +1,13 @@
+package edu.ifsp.fichaLimpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaLimpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichaLimpaApplication.class, args);
+	}
+
+}
---
+++ fichaLimpa/src/test/java/edu/ifsp/fichaLimpa/FichaLimpaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifsp.fichaLimpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaLimpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
