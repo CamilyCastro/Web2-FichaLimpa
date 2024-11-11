330ece61a752ebf147bbd39dd402c2bce0efad7b
Pkaynan <pietrokaynan2@gmail.com>
2024-11-11 10:49:22 -0300

    adicionando partido ao politico

--- fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/controller/PoliticoController.java
+++ fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/controller/PoliticoController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import edu.ifsp.fichaLimpa.model.Partido;
 import edu.ifsp.fichaLimpa.model.Politico;
+import edu.ifsp.fichaLimpa.repositorios.PartidoRepositorio;
 import edu.ifsp.fichaLimpa.repositorios.PoliticoRepositorio;
 
 
@@ -16,6 +19,9 @@ public class PoliticoController {
 	
 	@Autowired
 	private PoliticoRepositorio politicoRepo; 
+	
+	@Autowired
+	private PartidoRepositorio partidoRepo;
 
 	@ModelAttribute(name = "politico")
 	public Politico politico() {
@@ -29,14 +35,23 @@ public class PoliticoController {
 	}
 
 	@PostMapping
-	public String executarCadastroPolitico(@Valid Politico politico, Errors errors){
+	public String executarCadastroPolitico(@Valid Politico politico, @RequestParam("idPartido") Long idPartido ,
+			Errors errors){
+		
 		if (errors.hasErrors()){
 			return "politico-form";
 		}
+		
+		Partido partido = partidoRepo.findById(idPartido).orElse(null);
+		
+		if(partido == null) {
+			return "politico-form";
+		}
 
+		politico.setPartido(partido);
 		politicoRepo.save(politico);
 
-		return "redirect/(nome do end point de partidoController)";
+		return "/home";
 	}
 
 }
