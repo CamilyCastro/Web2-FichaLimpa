e5f139691b2e24fced87b818d5c9088938b721dc
camily <camily@ifsp.com>
2024-11-11 10:42:48 -0300

    crud_partido completo

--- fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/controller/PartidoController.java
+++ fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/controller/PartidoController.java
@@ -2,13 +2,19 @@ package edu.ifsp.fichaLimpa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import edu.ifsp.fichaLimpa.model.Partido;
 import edu.ifsp.fichaLimpa.repositorios.PartidoRespositorio;
+import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("politico")
 @RequestMapping("/partido")
 public class PartidoController {
 	
@@ -20,7 +26,19 @@ public class PartidoController {
 		return new Partido();
 	}
 	
+	@GetMapping
 	public String viewPartido() {
 		return "partido-form";
+	}
+	
+	@PostMapping
+	public String cadastroPartido(@Valid Partido partido, Errors errors){
+		if (errors.hasErrors()){
+			return "partido-form";
+		}
+		
+		partidoRepo.save(partido);
+		
+		return("redirect:/");
 	}
 }
