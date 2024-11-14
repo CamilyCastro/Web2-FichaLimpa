fe227eb7c108470ceb8eac8164066d6862c126d4
Amanda <acampezan@gmail.com>
2024-11-13 22:44:15 -0300

    CRUD cidadão

---
+++ fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/controller/CidadaoController.java
@@ -0,0 +1,60 @@
+package edu.ifsp.fichaLimpa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import edu.ifsp.fichaLimpa.model.Cidadao;
+import edu.ifsp.fichaLimpa.model.Endereco;
+import edu.ifsp.fichaLimpa.repositorios.CidadaoRepositorio;
+import edu.ifsp.fichaLimpa.repositorios.EnderecoRepositorio;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@SessionAttributes("cidadao")
+@RequestMapping("/cidadao")
+public class CidadaoController {
+
+	@Autowired
+	private CidadaoRepositorio cidadaoRepositorio;
+	
+	@Autowired
+	private EnderecoRepositorio enderecoRepositorio;
+	
+	@ModelAttribute(name = "cidadao")
+	public Cidadao cidadao() {
+		return new Cidadao();
+	}
+	
+	public Endereco endereco() {
+		return new Endereco();
+	}
+	
+	@GetMapping
+	public String viewCidadao() {
+		return "cidadao-form";
+	}
+	
+	@PostMapping
+	public String salvarCidadao(@Valid @ModelAttribute Cidadao cidadao, Errors errors) {
+		if(errors.hasErrors()) {
+			return "cidadao-form";
+		}
+		
+		if (cidadao.getEndereco() != null) {
+	        cidadao.getEndereco().setCidadao(cidadao);
+	    }
+//		enderecoRepositorio.save(endereco);
+//		cidadao.setEndereco(endereco);
+		cidadaoRepositorio.save(cidadao);
+		
+		return "redirect:/";
+	}
+}
--- fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/model/Politico.java
+++ fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/model/Politico.java
@@ -1,5 +1,7 @@
 package edu.ifsp.fichaLimpa.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -41,10 +42,11 @@ public class Politico {
 
 	@NotBlank(message = "Obrigatório informar a data de nascimento.")
 	@Pattern(regexp = "\\d{2}/(0[1-9]|1[0-2])/\\d{4}", message = "Data de nascimento deve estar no formato DD/MM/AAAA")
-	private String dataNasc;
+	@Column(name = "data_nascimento")
+	private Date dataNasc;
 	
 	@NotBlank(message = "Obrigatório informar o contato.")
-	private int contato;
+	private String contato;
 
 	@ManyToOne
 	@JoinColumn(name = "id_partido")
