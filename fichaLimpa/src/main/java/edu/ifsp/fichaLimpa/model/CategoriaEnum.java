388fa2deb51d7b309784d9751fbc0d1f23332970
Pkaynan <pietrokaynan1@gmail.com>
2024-12-16 15:57:37 -0300

    categoria melhorada

--- fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/model/CategoriaEnum.java
+++ fichaLimpa/src/main/java/edu/ifsp/fichaLimpa/model/CategoriaEnum.java
@@ -1,10 +1,20 @@
 package edu.ifsp.fichaLimpa.model;
 
+import lombok.Getter;
+
 public enum CategoriaEnum {
 
-	SAUDE, 
-	TECNOLOGIA, 
-	ECONOMIA, 
-	SOCIAL,
-	SEGURANCA;
+	SAUDE("Saúde"),
+	TECNOLOGIA("Tecnologia"),
+	ECONOMIA("Economia"),
+	SOCIAL("Social"),
+	SEGURANCA("Segurança");
+
+	@Getter
+	private final String descricao;
+
+	CategoriaEnum(String descricao){
+		this.descricao = descricao;
+	}
+
 }
